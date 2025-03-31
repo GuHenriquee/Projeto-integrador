2f91a5635bb6d2e5faaf186096d73855ebcbdf34
Gustavo <gguu.henri@gmail.com>
2025-03-31 16:22:02 -0300

    Cadastro de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from "./formulario/formulario.component";
+import { CabecalhoComponent } from "./cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormularioComponent,
+    FormularioComponent,
+    CabecalhoComponent,
+    CabecalhoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'projeto-integrador';
 }
