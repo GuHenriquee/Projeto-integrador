d8ed913bb412f8fd33fe3f6c74b67fe8f78967b2
Gustavo <gguu.henri@gmail.com>
2025-03-31 23:27:52 -0300

    teste de criação de novo evento

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
 
+export class FormularioComponent {
+    nome_evento: string;
+    id: string;
+    descricao: string;
+    data_criacao: Date;
+    endereco: string;
+    preco_ingresso: number;
+    tipo_evento: string;
+    status: boolean;
+    data_evento: Date;
+
+    constructor() {
+        this.nome_evento = "";
+        this.descricao = "";
+        this.preco_ingresso = 0;
+        this.endereco = "";
+        this.tipo_evento = "";
+        this.status = false;
+        this.data_evento = new Date();
+        this.id = crypto.randomUUID();
+        this.data_criacao = new Date();
+    }
 }
+
+const evento = new FormularioComponent();
+evento.nome_evento = "Show do Coldplay";
+evento.descricao = "Concerto de rock internacional";
+evento.endereco = "Estádio do Maracanã, RJ";
+evento.preco_ingresso = 250.00;
+evento.tipo_evento = "Show";
+evento.data_evento = new Date("2024-12-15");
+
+console.log(evento);
+
+
