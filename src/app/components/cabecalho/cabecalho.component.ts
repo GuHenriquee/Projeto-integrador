1f832975f7fbf3154c54b9a507b621a75ffd71c9
Gustavo <gguu.henri@gmail.com>
2025-06-10 22:16:16 -0300

    login

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { SupabaseService } from '../../services/supabase.service';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cabecalho',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.css'
 })
@@ -24,7 +25,19 @@ export class CabecalhoComponent {
         }
   }
 
-    constructor(private supabaseService: SupabaseService){}
+    authForm: FormGroup;
+    isLogin: boolean = true;
+
+constructor(private fb: FormBuilder,
+      private supabaseService: SupabaseService,
+      private router: Router){
+        this.authForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]]
+      });
+    }
+
+
     estaLogado = false;
      async logout() {
         
@@ -39,5 +52,34 @@ export class CabecalhoComponent {
     } else {
       console.log('Usuário NÃO está logado');
     }
+  
+    }
+  
+    async onSubmit() {
+    const email = this.authForm.value.email;
+    const senha = this.authForm.value.senha;
+  
+    console.log('Email:', email);
+  console.log('Senha:', senha);
+    try {
+      const { data, error } = await this.supabaseService.signIn(email, senha);
+  
+      if (error) {
+        throw error;
+      }
+  
+      console.log('Login bem-sucedido!', data);
+      this.router.navigate(['/home']); // Redirecione se necessário
+    } catch (err: any) {
+      console.error('Erro de autenticação:', err.message || err);
+      // Aqui você pode exibir um alerta ou mensagem de erro ao usuário
+    }
   }
+  
+    alternarModo() {
+      this.isLogin = !this.isLogin;
+    }
+
 }
+
+
